dfbdd4f92c4fbef75b66be63bccae645cb2bd4c7
oualid <o-oualid@outlook.com>
2022-04-13 17:25:26 +0000

    fix binding for non taxable area

---
+++ TaxesV1/MultiValueConverters/DifferenceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using TaxesV1.Resources;
+
+namespace TaxesV1.MultiValueConverters
+{
+    public class DifferenceConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is double && values[1] is double)
+            {
+                return ((double)values[0] - (double)values[1]).ToString();
+            }
+
+            return "";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new object[] { };
+        }
+    }
+}

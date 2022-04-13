07088eaec69c499100215814337d653000a678c3
oualid <o-oualid@outlook.com>
2022-04-13 02:31:15 +0000

    add basic styling

--- TaxesV1/TaxesSureTNB.xaml.cs
+++ TaxesV1/TaxesSureTNB.xaml.cs
@@ -8,11 +8,14 @@ namespace TaxesV1
     public partial class TaxesSureTNB : Window
     {
         private TaxesV2Entities Entities = new TaxesV2Entities();
+        private CollectionViewSource ViewSource = new CollectionViewSource();
         public TaxesSureTNB()
         {
             InitializeComponent();
             if(Properties.Settings.Default.Language=="ar")SetFlowDirection(Body,FlowDirection.RightToLeft);
-         //   DataGrid.ItemsSource = Entities.Dossiers.ToList();
+            DataContext = Entities.Declarations;
+           // ViewSource.Source = Entities.Declarations;
+            //   DataGrid.ItemsSource = Entities.Dossiers.ToList();
         }
         
         private void ButtonFetch_Click(object sender, RoutedEventArgs e)
